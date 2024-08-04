c534e78848ce88e75733f4fffb5d38f5db2d41bf
Sc1pex <scipex.dev@gmail.com>
2024-08-04 13:43:51 +0300

    test bignum_add with random values

--- cli/src/main.c
+++ cli/src/main.c
@@ -6,21 +6,6 @@
 #include "fib_impl.h"
 #include "run.h"
 
-void test_random_bignum(int count) {
-    for (int i = 0; i < count; i++) {
-        uint64_t n1 = rand();
-        uint64_t n2 = rand();
-        uint64_t nsum = n1 + n2;
-
-        BigNum b1 = bignum_fromu64(n1);
-        BigNum b2 = bignum_fromu64(n2);
-        BigNum bsum = bignum_add(b1, b2);
-        BigNum bsum1 = bignum_fromu64(nsum);
-
-        assertf(bignum_eq(bsum, bsum1), "");
-    }
-}
-
 int main(int argc, char** argv) {
     Cli cli;
     if (!cli_parse(&cli, argc, argv)) {
--- fib_lib/test/num.cpp
+++ fib_lib/test/num.cpp
@@ -1,5 +1,24 @@
 #include <gtest/gtest.h>
+#include <random>
 
-TEST(test, simple) {
-    EXPECT_EQ(14, 14);
+extern "C" {
+#include "num.h"
+}
+
+TEST(BigNum, random_sum) {
+    int count = 10000000;
+    std::mt19937 gen(time(nullptr));
+
+    for (int i = 0; i < count; i++) {
+        uint64_t n1 = gen();
+        uint64_t n2 = gen();
+        uint64_t nsum = n1 + n2;
+
+        BigNum b1 = bignum_fromu64(n1);
+        BigNum b2 = bignum_fromu64(n2);
+        BigNum bsum = bignum_add(b1, b2);
+        BigNum bsum1 = bignum_fromu64(nsum);
+
+        ASSERT_TRUE(bignum_eq(bsum, bsum1));
+    }
 }

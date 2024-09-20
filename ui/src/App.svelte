2a756617ff95732c32e58440451ea6c069685f66
Sc1pex <scipex.dev@gmail.com>
2024-09-20 09:32:05 +0000

    start of svelte ui

---
+++ ui/src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    const options = ["CNaive", "CMatrix"];
+    let selected: string;
+    let num: number;
+
+    let result: { num: string; ms: number } | undefined = undefined;
+
+    function handleSubmit() {
+        fetch(`/api/calculate/${selected}/${num}`).then(async (resp) => {
+            const res = await resp.json();
+            result = res;
+        });
+    }
+</script>
+
+<form
+    class="flex flex-col items-center pt-20 gap-5"
+    on:submit|preventDefault={handleSubmit}
+>
+    <select class="bg-zinc-600 p-2 rounded" bind:value={selected}>
+        {#each options as option}
+            <option value={option}>
+                {option}
+            </option>
+        {/each}
+    </select>
+
+    <input
+        class="bg-zinc-600 w-16 p-2 rounded"
+        type="number"
+        bind:value={num}
+        min="0"
+    />
+
+    <button type="submit"> Calculate Result </button>
+</form>
